cb17fc6fb5eba15354005433dbbfbbeb7b25eabc
sobhanhadikhah <hadikhahs@gmail.com>
2023-05-21 17:01:15 +0330

    add some new style and some new thing

--- client/src/components/lobby.jsx
+++ client/src/components/lobby.jsx
@@ -1,7 +1,7 @@
 import { useState, useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSocket } from "../context/SocketProvider";
-
+import { BsTelephone } from "react-icons/bs";
 
 export default function Lobby() {
     const [email, setEmail] = useState("");
@@ -33,27 +33,32 @@ export default function Lobby() {
         };
     }, [socket, handleJoinRoom]);
     return (
-        <div>
-            <h1>Lobby</h1>
-            <form onSubmit={handleSubmitForm}>
-                <label htmlFor="email">Email ID</label>
-                <input
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <br />
-                <label htmlFor="room">Room Number</label>
-                <input
-                    type="text"
-                    id="room"
-                    value={room}
-                    onChange={(e) => setRoom(e.target.value)}
-                />
-                <br />
-                <button>Join</button>
-            </form>
+        <div className=" h-screen text-black" >
+            <h1 className="items-center" ><span className="bg-[#0282F9] text-white p-3 items-center flex flex-row gap-3 text-3xl " >Way2Connect  <span className="text-lg" >Video Chat</span> </span> </h1>
+            <div className="grid place-content-center h-[50vh] text-center items-center justify-center" >
+
+                <form onSubmit={handleSubmitForm} className=" bg-[#0282F9] p-3 rounded-md text-white flex flex-col justify-center items-center " >
+                    <label htmlFor="email">Email ID</label>
+                    <input
+                        className="bg-transparent   px-2 w-[300px]  outline-none  border-b-2 "
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <br />
+                    <label htmlFor="room">Room Number</label>
+                    <input
+                        className="bg-transparent w-full px-2  outline-none  border-b-2 "
+                        type="text"
+                        id="room"
+                        value={room}
+                        onChange={(e) => setRoom(e.target.value)}
+                    />
+                    <br />
+                    <button className="absolute bottom-0 text-white  bg-[#0282F9] py-3 my-4 rounded-full px-3  " ><BsTelephone /></button>
+                </form>
+            </div>
         </div>
     )
 }
--- client/src/components/room.jsx
+++ client/src/components/room.jsx
@@ -2,13 +2,14 @@ import { useEffect, useCallback, useState } from "react";
 import ReactPlayer from "react-player";
 import peer from "../service/peer";
 import { useSocket } from "../context/SocketProvider";
-
+import { AiFillSound } from "react-icons/ai";
 
 export default function Room() {
     const socket = useSocket();
     const [remoteSocketId, setRemoteSocketId] = useState(null);
     const [myStream, setMyStream] = useState();
     const [remoteStream, setRemoteStream] = useState();
+    const [mutedVoice, setMutedVoice] = useState(false);
 
     const handleUserJoined = useCallback(({ email, id }) => {
         console.log(`Email ${email} joined room`);
@@ -110,35 +111,49 @@ export default function Room() {
         handleNegoNeedFinal,
     ]);
     return (
-        <div>
-            <h1>Room Page</h1>
-            <h4>{remoteSocketId ? "Connected" : "No one in room"}</h4>
-            {myStream && <button onClick={sendStreams}>Send Stream</button>}
-            {remoteSocketId && <button onClick={handleCallUser}>CALL</button>}
-            {myStream && (
-                <>
-                    <h1>My Stream</h1>
-                    <ReactPlayer
-                        playing
-                        muted
-                        height="100px"
-                        width="200px"
-                        url={myStream}
-                    />
-                </>
-            )}
-            {remoteStream && (
-                <>
-                    <h1>Remote Stream</h1>
-                    <ReactPlayer
-                        playing
-                        muted
-                        height="100px"
-                        width="200px"
-                        url={remoteStream}
-                    />
-                </>
-            )}
+        <div   >
+            <h1 className="items-center" ><span className="bg-[#0282F9] text-white p-3 items-center flex flex-row gap-3 text-3xl " >Way2Connect  <span className="text-lg" >Video Chat</span> </span> </h1>
+            <div className="flex flex-col " >
+                <h4 className="text-center text-3xl py-3  " >{remoteSocketId ? <span className="text-green-500" >Connected</span> : <span className="text-red-600" >No one in room</span>}</h4>
+                {remoteSocketId && <button className="bg-green-500  text-white p-3 rounded-md" onClick={handleCallUser}>Accept</button>}
+            </div>
+            <div className="flex flex-col md:flex-row items-center justify-center pt-8  " >
+
+                {myStream && (
+                    <div className="flex flex-col justify-center items-center player-wrapper  " >
+                        <ReactPlayer
+                            className="react-player"
+                            playing
+                            muted
+                            url={myStream}
+                            volume={1}
+                            controls
+                        />
+                        <h1>My Camera</h1>
+                    </div>
+                )}
+                {remoteStream && (
+                    <div className="flex flex-col justify-center items-center ">
+                        <div className="flex flex-col  player-wrapper  justify-center items-center" >
+
+
+                            <ReactPlayer
+                                className="react-player"
+                                playing
+                                volume={1}
+                                muted={mutedVoice}
+                                url={remoteStream}
+                            />
+
+                            <AiFillSound onClick={() => setMutedVoice(!mutedVoice)} className={` cursor-pointer ${mutedVoice ? "text-red-500" : "text-green-400"}   `} size={30} />
+                        </div>
+                        <h1>User camera</h1>
+                    </div>
+                )}
+            </div>
+            <div className="absolute bottom-0  left-0 right-0 " >
+                {myStream && <button className="bg-slate-200 p-3" onClick={sendStreams}>Show All Camera</button>}
+            </div>
         </div>
     )
 }
