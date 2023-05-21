f6431bbdf5ee1378ba512667044bc226748d56ab
sobhanhadikhah <hadikhahs@gmail.com>
2023-05-21 15:17:24 +0330

    none style

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Lobby from "./components/lobby";
+import Room from "./components/room";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Lobby />} />
+        <Route path="/room/:roomId" element={<Room />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import { SocketProvider } from "./context/SocketProvider.jsx";
+import reportWebVitals from "./reportWebVitals.js";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <SocketProvider>
+        <App />
+      </SocketProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
